007f2ee2c636d9fc31a011f8897ec742f0c0c336
Jahnavi-51 <bejawadajahnavi20@gmail.com>
2024-07-31 11:15:54 +0530

    First commit

---
+++ second.py
@@ -0,0 +1,30 @@
+name = 'janu'
+print(hex(id(name)))
+str = '\'Hello!\' hey i am janu '
+print(str)
+str1 = "WASTE FELLOW"
+str2 = "good mrng"
+print(name[3])
+for i in name:
+    if (i == 'j') :
+        print(i)
+# formatting
+str45 = 'WHAT WHY HOW'
+print(f"{1}",str45)
+str3 = str1 + ' ' +str2
+print(str3)
+print("this is from \"Batch-1\" training")
+print(str1.lower())
+print(str2.upper())
+x=2986475322
+print(f"my phone no is : {x}")
+x=y=z='vasavi'
+a,b,c = 'apple', 'ball', 'cat'
+print(a,b,c)
+print(x,y,z)
+print(str1[7])
+print(str1[3:])
+print(str1[-1])
+print(str1[-8])
+#reverse
+print(str1[::-1])
